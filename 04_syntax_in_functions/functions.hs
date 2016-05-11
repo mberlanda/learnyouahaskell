05019834fff0e9d7106953f8e45f1e3140754fcf
Mauro Berlanda <m.berlanda@fit2you.it>
2016-05-11 18:14:51 +0200

    04 syntax in function

--- 04_syntax_in_functions/functions.hs
+++ 04_syntax_in_functions/functions.hs
@@ -21,3 +21,49 @@ addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 capital :: String -> String  
 capital "" = "Empty string, whoops!"  
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- Guards, guards!
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT  
+
+-- Where!?
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"  
+    | otherwise                 = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs]  
+    where bmi weight height = weight / height ^ 2
+
+-- Let it be
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis' xs = [bmi w h | (w, h) <- xs, let bmi w h = w / h ^ 2]
+
+-- Case expressions
+head' :: [a] -> a  
+head' xs = case xs of [] -> error "No head for empty lists!"  
+                      (x:_) -> x
+
+describeList' :: [a] -> String  
+describeList' xs = "The list is " ++ case xs of [] -> "empty."  
+                                                [x] -> "a singleton list."   
+                                                xs -> "a longer list."
+
+describeList'' :: [a] -> String  
+describeList'' xs = "The list is " ++ what xs  
+    where what [] = "empty."  
+          what [x] = "a singleton list."  
+          what xs = "a longer list."
--- 04_syntax_in_functions/syntax_in_functions.md
+++ 04_syntax_in_functions/syntax_in_functions.md
@@ -1,16 +1,30 @@
 # Syntax in Functions
 
 #### Pattern matching
-```
+```bash
 ghci> let xs = [(1,3), (4,3), (2,4), (5,3), (5,6), (3,1)]  
 ghci> [a+b | (a,b) <- xs]  
 [4,7,6,8,11,4]  
-
 ```
-#### Guards, guards!
-
-#### Where!?
 
 #### Let it be
-
+```bash
+ghci> [let square x = x * x in (square 5, square 3, square 2)]  
+[(25,9,4)]
+ghci> (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)  
+(6000000,"Hey there!")
+ghci> let zoot x y z = x * y + z  
+ghci> zoot 3 9 2  
+29  
+ghci> let boot x y z = x * y + z in boot 3 4 2  
+14  
+ghci> boot  
+<interactive>:1:0: Not in scope: `boot'
+```
 #### Case expressions
+```hs
+case expression of pattern -> result  
+                   pattern -> result  
+                   pattern -> result  
+                   ...  
+```
